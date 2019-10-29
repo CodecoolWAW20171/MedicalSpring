47a5dd0a0fbff0452682249a584c9ec1a06203d4
Los4U <inz.kamil.wos@gmail.com>
2019-10-29 02:36:34 +0100

    add responsive, details,scroller, pagin,

--- src/main/resources/static/js/searchPatient.js
+++ src/main/resources/static/js/searchPatient.js
@@ -1,139 +1,146 @@
-$(document).ready(function () {
-    $("#searchPatient").val($(".dataTables_filter input").val());
+// $(document).ready(function () {
 
+var startTime = Date.now();
 
-    $("#tablepage").dataTable({
-        responsive: true,
-        // deferRender: true,
-        "bStateSave": true,
-        "bSortClasses": false,
-        paging: false,
-        scrollY: "70vh",
-        fixedHeader: {
-            header: true,
-            footer: false
-        },
+$("#tablepage").dataTable({
+    responsive: {
+        details: {
+            type: 'column'
+        }
+    },
+    "bStateSave": true,
+    "bSortClasses": false,
+    paging: true,
+    deferRender:    true,
+    scrollY: "70vh",
+    scroller:       true,
+    "oLanguage": {
+        "sEmptyTable": "Brak danych do wyświetlenia",
+        "sInfo": "Znaleziono _TOTAL_ pacjentów (_START_ to _END_)",
+        "sInfoEmpty": "Nie znaleziono pacjentów",
+        "sInfoFiltered": " - z wszystkich _MAX_ pacjentów",
+        "sLoadingRecords": "Proszę czekać - ładowanie bazy pacjentów...",
         "oLanguage": {
-            "sEmptyTable": "Brak danych do wyświetlenia",
-            "sInfo": "Znaleziono _TOTAL_ pacjentów (_START_ to _END_)",
-            "sInfoEmpty": "Nie znaleziono pacjentów",
-            "sInfoFiltered": " - z wszystkich _MAX_ pacjentów",
-            "sLoadingRecords": "Proszę czekać - ładowanie bazy pacjentów...",
-            "oLanguage": {
-                "sProcessing": "Proszę czekać"
+            "sProcessing": "Proszę czekać"
+        }
+
+    },
+
+    ajax: {
+        url: '/patientsList',
+        dataSrc: ''
+    },
+    columns: [
+        {data: 'patientId'},
+        {data: 'patientId'},
+        {data: 'pesel'},
+        {data: 'surname'},
+        {data: 'name'},
+        {data: 'birthday'},
+        {data: 'sex'},
+        {data: 'zipCode'},
+        {data: 'city'},
+        {data: 'street'},
+        {data: 'houseNumber'},
+        {data: 'apartmentNumber'},
+
+        {data: 'patientId'},
+        {data: 'patientId'},
+        {data: 'patientId'},
+        // {data: 'patientId'},
+
+    ],
+    initComplete: function () {
+        console.log('DT init complete in ', Date.now() - startTime + ' milliseconds.');
+    },
+
+    "columnDefs": [
+        {
+            className: 'control',
+            orderable: false,
+            targets:   0
+        },
+        {
+            "targets": 0,
+            "data": "patientId",
+            "render": function (data, type, row, meta) {
+                return '<span></span>'
             }
-
         },
 
-        ajax: {
-            url: '/patientsList',
-            dataSrc: ''
+        {"orderable": false, "targets": [0, 9, 10, 11, 12, 13]},
+
+        {"searchable": false, "targets": [0,9, 10, 11, 12, 13]},
+
+        {
+            "targets": [-1, -2, -3],
+            "createdCell": function (td, cellData, rowData, row, col) {
+                $(td).css({'padding': '2px', 'text-align': 'center'})
+            }
         },
-        columns: [
-            {data: 'patientId'},
-            {data: 'pesel'},
-            {data: 'surname'},
-            {data: 'name'},
-            {data: 'birthday'},
-            {data: 'sex'},
-            {data: 'zipCode'},
-            {data: 'city'},
-            {data: 'street'},
-            {data: 'houseNumber'},
-            {data: 'apartmentNumber'},
-
-            {data: 'patientId'},
-            {data: 'patientId'},
-            {data: 'patientId'},
-            // {data: 'patientId'},
-
-        ],
-
-
-
-
-
-        "columnDefs": [
-            // { responsivePriority: 1, targets: 0 },
-            // { responsivePriority: 2, targets: 1 },
-            // { responsivePriority: 3, targets: 2 },
-
-            {
-                "targets": [-1, -2, -3],
-                "createdCell": function (td, cellData, rowData, row, col) {
-                    $(td).css({'padding': '2px', 'text-align': 'center'})
-                }
-            },
-            {
-                "targets": [-6, -7],
-                "createdCell": function (td, cellData, rowData, row, col) {
-                    $(td).css({'padding': '2px'})
-                }
-            },
-            {
-                "targets": [0],
-                "createdCell": function (td, cellData, rowData, row, col) {
-                    $(td).css({'padding': '3px', 'text-align': 'center'})
-                }
-            },
-            {
-                "targets": 0,
-                "data": "patientId",
-                "searchable": false,
-                "render": function (data, type, row, meta) {
-                    return '<a href="/patients/showPatientDetails?patientIdDetails=' + data + '"><span><i class="fas fa-user"></i> </span> <span>' + data + '</span></a>'
-                }
-            },
-            {
-                "targets": 11,
-                "data": "patientId",
-                "searchable": false,
-                "orderable": false,
-                "render": function (data, type, row, meta) {
-                    return '<a href="mails/show-mail-form?patientId=' + data + '">  <span style="font-size: 1em; color: cornflowerblue;">  <i class="fas fa-envelope"></i> </span> </a>';
-                }
-            },
-            {
-                "targets": 12,
-                "data": "patientId",
-                "searchable": false,
-                "orderable": false,
-                "render": function (data, type, row, meta) {
-                    return '<a href="/visits/showFormForAddVisit?patientId=' + data + '">  <span style="font-size: 1em; color: lightseagreen;">  <i class="fas fa-user-nurse"></i> </span> </a>';
-                }
-            },
-            {
-                "targets": 13,
-                "data": "patientId",
-                "searchable": false,
-                "orderable": false,
-                "render": function (data, type, row, meta) {
-                    return '<a href="/patients/showFormForEditPatient?patientIdToEdit=' + data + '">  <span style="font-size: 1em; color: lightslategray;">  <i class="fas fa-user-edit"></i> </span> </a>';
-                }
-            },
-            // {
-            //     "targets": 14,
-            //     "data": "patientId",
-            //     "searchable": false,
-            //     "orderable": false,
-            //     "render": function (data, type, row, meta) {
-            //         return '<a  onclick="if(!(confirm(\'Czy jesteś pewien?\')))return false" href="/patients/delete?patientIdToDelete=' + data + '">  <span style="font-size: 1em; color: black;">  <i class="fas fa-trash-alt"></i> </span> </a>';
-            //     }
-            // }
-        ]
-    });
-
-    $("#searchPatient").on("input change paste keyup", function (e) {
-        e.preventDefault();
-        $('#tablepage').DataTable().search($(this).val()).draw();
-    });
-
-    $("#resetInput").on("click", function (e) {
-        e.preventDefault();
-        $("#searchPatient").val("");
-        $(".dataTables_filter input").val("")
-        $('#tablepage').DataTable().search($("#searchPatient").val()).draw();
-    });
-
-
+        {
+            "targets": [-4, -5, -6, -7],
+            "createdCell": function (td, cellData, rowData, row, col) {
+                $(td).css({'padding': '2px'})
+            }
+        },
+        {
+            "targets": [1],
+            "createdCell": function (td, cellData, rowData, row, col) {
+                $(td).css({'padding': '3px', 'text-align': 'center'})
+            }
+        },
+        {
+            "targets": 1,
+            "data": "patientId",
+            "render": function (data, type, row, meta) {
+                return '<a href="/patients/showPatientDetails?patientIdDetails=' + data + '"><span><i class="fas fa-user"></i> </span> <span>' + data + '</span></a>'
+            }
+        },
+        {
+            "targets": 12,
+            "data": "patientId",
+            "render": function (data, type, row, meta) {
+                return '<a href="mails/show-mail-form?patientId=' + data + '">  <span style="font-size: 1em; color: cornflowerblue;">  <i class="fas fa-envelope"></i> </span> </a>';
+            }
+        },
+        {
+            "targets": 13,
+            "data": "patientId",
+            "render": function (data, type, row, meta) {
+                return '<a href="/visits/showFormForAddVisit?patientId=' + data + '">  <span style="font-size: 1em; color: lightseagreen;">  <i class="fas fa-user-nurse"></i> </span> </a>';
+            }
+        },
+        {
+            "targets": 14,
+            "data": "patientId",
+            "render": function (data, type, row, meta) {
+                return '<a href="/patients/showFormForEditPatient?patientIdToEdit=' + data + '">  <span style="font-size: 1em; color: lightslategray;">  <i class="fas fa-user-edit"></i> </span> </a>';
+            }
+        },
+        // {
+        //     "targets": 14,
+        //     "data": "patientId",
+        //
+        //     "render": function (data, type, row, meta) {
+        //         return '<a  onclick="if(!(confirm(\'Czy jesteś pewien?\')))return false" href="/patients/delete?patientIdToDelete=' + data + '">  <span style="font-size: 1em; color: black;">  <i class="fas fa-trash-alt"></i> </span> </a>';
+        //     }
+        // }
+    ]
 });
+
+$("#searchPatient").on("input change paste keyup", function (e) {
+    e.preventDefault();
+    $('#tablepage').DataTable().search($(this).val()).draw();
+});
+
+$("#resetInput").on("click", function (e) {
+    e.preventDefault();
+    $("#searchPatient").val("");
+    $(".dataTables_filter input").val("")
+    $('#tablepage').DataTable().search($("#searchPatient").val()).draw();
+});
+
+$("#searchPatient").val($(".dataTables_filter input").val());
+
+// });
